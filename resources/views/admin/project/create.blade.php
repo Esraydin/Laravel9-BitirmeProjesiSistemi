54327e36873a4b4e4b1840fac68b42b5b3d710c7
Esraydin <aydinesra000@gmail.com>
2022-05-18 13:01:38 +0300

    Summernote Implementation

---
+++ resources/views/admin/project/create.blade.php
@@ -0,0 +1,94 @@
+@extends('layouts.adminbase')
+
+@section('title', 'Add Category')
+@section('head')
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+@endsection
+
+
+@section('content')
+
+    <div class="page-header">
+        <h3 class="page-title">Add Category</h3>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{route('admin.index')}}">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Category List</li>
+            </ol>
+        </nav>
+    </div>
+
+
+    <div class="col-12 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Category Elements</h4>
+
+                <form class="form" action="{{route('admin.project.store')}}" method="post" enctype="multipart/form-data">
+                    @csrf
+
+
+                    <div class="form-group">
+                     <label >Parent Category</label>
+                        <select class="form-control select2" name="category_id" style ="">
+                            <option value="0" selected="selected">Main Category</option>
+                            @foreach($data as $rs)
+                                <option value="{{$rs->id}}">{{\App\Http\Controllers\AdminPanel\CategoryController::getParentsTree($rs,$rs->title)}}</option>
+                            @endforeach
+                        </select>
+
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail3">Title</label>
+                        <input type="text" class="form-control" name="title" placeholder="Title">
+
+                    </div>
+
+                    <div class="form-group">
+                        <label for="exampleInputEmail3">Keywords</label>
+                        <input type="text" class="form-control" name="keywords" placeholder="Keywords">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail3">Description</label>
+                        <input type="text" class="form-control" name="Description" placeholder="Description">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail3">Detail</label>
+                        <textarea id="summernote" name="detail"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label>File upload</label>
+                        <input type="file" name="img[]" class="file-upload-default">
+                        <div class="input-group col-xs-12">
+                            <input type="file" class="form-control file-upload-info" disabled="" placeholder="Upload Image">
+                            <span class="input-group-append">
+                            <label class="file-upload-browse btn btn-gradient-primary" >Upload</label>
+                          </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>Status</label>
+                        <select class="form-control form-control-lg " name="status">
+                            <option>True</option>
+                            <option>False</option>
+
+                        </select>
+                    </div>
+
+                        <button type="submit" class="btn btn-gradient-primary me-2">Save</button>
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
+@section('foot')
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#summernote').summernote();
+        });
+    </script>
+@endsection
---
+++ resources/views/admin/project/show.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.adminbase')
+
+@section('title', 'Show Category: '.$data->title)
+
+
+
+@section('content')
+    <div class="page-header">
+        <h3 class="page-title"></h3>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{route('admin.index')}}">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Show Category</li>
+            </ol>
+        </nav>
+    </div>
+       <div class ="row mb-2">
+           <div class="col-sm-3">
+               <a href="{{route('admin.category.edit',['id'=>$data->id])}}" class="btn btn-gradient-info btn-rounded btn-fw" style="width:100px">edit</a>
+
+           </div>
+
+
+        <div class="col-sm-3">
+            <a href="{{route('admin.category.destroy',['id'=>$data->id])}}" onclick="return confirm('Deleting !! Are you sure?')" class="btn btn-gradient-danger btn-rounded btn-fw" style="width:100px">
+                delete</a>
+        </div>
+
+       </div>
+
+       <div class="card">
+            <div class="card-body">
+            <form class="form" action="/admin/category/update/{{$data->id}}" method="post">
+                    @csrf
+
+                    <div class="card">
+
+                            <h3 class="card-title">Detail Data</h3>
+
+                        <div class="card-body p-0">
+                            <table class="table table-striped">
+
+                                <tr>
+                                    <th style="width: 70px">Id</th>
+                                    <td>{{$data->id}}</td>
+                                </tr>
+                                <tr>
+                                    <th>Title</th>
+                                    <td>{{$data->title}}</td>
+
+                                </tr>
+                                <tr>
+                                    <th>Keywords</th>
+                                    <td>{{$data->keywords}}</td>
+
+                                </tr>
+                                <tr>
+                                    <th>Image</th>
+                                    <td></td>
+
+                                </tr>
+                                <tr>
+                                    <th>Status</th>
+                                    <td>{{$data->status}}</td>
+
+                                </tr>
+                                <tr>
+                                    <th>Created Date</th>
+                                    <td>{{$data->created_at}}</td>
+
+                                </tr>
+                                <tr>
+                                    <th>Update Date</th>
+                                    <td>{{$data->updated_at}}</td>
+
+                                </tr>
+                            </table>
+                        </div>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+@endsection
