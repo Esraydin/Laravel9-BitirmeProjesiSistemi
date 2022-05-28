285d212167bc118f26cc74ba62c04d86223e5017
Esraydin <aydinesra000@gmail.com>
2022-05-28 14:07:30 +0300

    Project Image Gallery

--- resources/views/admin/image/index.blade.php
+++ resources/views/admin/image/index.blade.php
@@ -6,41 +6,57 @@
 
 @section('content')
 
-    <div class="page-header">
-        <h3 class="page-title"></h3>
-          <div class="card-body">
-            <table class="table table-bordered">
-                <h3 class="page-title">Project Image List</h3>
-                <thead>
-                <tr>
-                    <th style="width: 10px">Id</th>
-                    <th>Title</th>
-                    <th>Image</th>
-                    <th style="width: 40px">Update</th>
-                    <th style="width: 40px">Delete</th>
 
 
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($data as $rs)
+                <h2>{{$project->title}}</h2>
+                <form class="form" action="{{route('admin.image.store',['pid'=>$project->id])}}" method="post" enctype="multipart/form-data">
+                    @csrf
+
+
+                    <div class="form-group">
+                        <label>Title</label>
+
+                        <input type="text" class="form-control" name="title" placeholder="Title">
+                        <div class="input-group col-xs-12">
+                            <input type="file" class="form-control file-upload-info"  placeholder="Choose Image File" name="image">
+                            <span class="input-group-append">
+
+                          </span>
+                        </div>
+                    </div>
+
+                <button type="submit" class="btn btn-gradient-primary me-2">Save</button>
+
+                </form>
+<div class="page-header">
+    <h3 class="page-title"></h3>
+    <div class="card-body">
+        <table class="table table-bordered">
+            <h3 class="page-title">Project Image List</h3>
+            <thead>
+            <tr>
+                <th style="width: 10px">Id</th>
+                <th>Title</th>
+                <th>Image</th>
+                <th style="width: 40px">Delete</th>
+
+
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($images as $rs)
                 <tr>
                     <td>{{$rs->id}}</td>
                     <td>{{$rs->title}}</td>
                     <td>
                         @if($rs->image)
-                            <img src="{{Storage::url($rs->image)}}" style="height: 50px">
+                            <img src="{{Storage::url($rs->image)}}" style="height: 60px">
                         @endif
                     </td>
-                    <td><img src="{{asset('assets')}}/admin/assets/images/images.png" style="height: 50px" ></td>
 
-                    <td>{{$rs->status}}</td>
-                    <td><a href="{{route('admin.project.edit',['id'=>$rs->id])}}" class="gradient-info btn-rounded btn-fw">Edit</a> </td>
-                    <td><a href="{{route('admin.project.destroy',['id'=>$rs->id])}}" class="gradient-danger btn-rounded btn-fw"
+                    <td><a href="{{route('admin.image.destroy',['pid'=>$project->id,'id'=>$rs->id])}}" class="btn btn-gradient-danger btn-rounded btn-fw"
                            onclick="return confirm('Deleting !! Are you sure?')">Delete</a> </td>
 
-
-                </tr>
 
                 @endforeach
 
--- routes/web.php
+++ routes/web.php
@@ -51,40 +51,36 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 
 
 //.................... Admin Panel Routes...........................
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::get('/', [AdminHomeController::class, 'index'])->name('index');
+Route::prefix('admin')->name('admin.')->group(function (){
+Route::get('/', [AdminHomeController::class, 'index'])->name('index');
+
+
 // ....................Admin Category Routes........................
-    Route::prefix('/category')->name('category.')->controller(AdminCategoryController::class)->group(function () {
-        Route::get('/', 'index')->name('index');
-        Route::get('/create', 'create')->name('create');
-        Route::post('/store', 'store')->name('store');
-        Route::get('/edit/{id}', 'edit')->name('edit');
-        Route::post('/update/{id}', 'update')->name('update');
-        Route::get('/show/{id}', 'show')->name('show');
-        Route::get('/destroy/{id}', 'destroy')->name('destroy');
-    });
-
-
-
-//..................... Admin Project Routes........................
-       Route::prefix('/project')->name('project.')->controller(AdminProjectController::class)->group(function (){
-        Route::get('/', 'index')->name('index');
-        Route::get('/create', 'create')->name('create');
-        Route::post('/store', 'store')->name('store');
-        Route::get('/edit/{id}', 'edit')->name('edit');
-        Route::post('/update/{id}', 'update')->name('update');
-        Route::post('/show/{id}', 'show')->name('show');
-        Route::get('/destroy/{id}', 'destroy')->name('destroy');
-
-       });
-
-    //..................... Admin Project Image Gallery Routes........................
-    Route::prefix('/image')->name('image.')->controller(ImageController::class)->group(function (){
-        Route::get('/{pid}', 'index')->name('index');
-        Route::get('/create/{pid}', 'create')->name('create');
-        Route::post('/store/{pid}', 'store')->name('store');
-        Route::post('/update/{pid}/{id}', 'update')->name('update');
-        Route::get('/destroy/{pid}/{id}', 'destroy')->name('destroy');
-
-    });
+Route::prefix('/category')->name('category.')->controller(AdminCategoryController::class)->group(function () {
+    Route::get('/', 'index')->name('index');
+    Route::get('/create', 'create')->name('create');
+    Route::post('/store', 'store')->name('store');
+    Route::get('/edit/{id}', 'edit')->name('edit');
+    Route::post('/update/{id}', 'update')->name('update');
+    Route::get('/show/{id}', 'show')->name('show');
+    Route::get('/destroy/{id}', 'destroy')->name('destroy');
 });
+// ....................Admin Project Routes........................
+Route::prefix('/project')->name('project.')->controller(AdminProjectController::class)->group(function () {
+    Route::get('/', 'index')->name('index');
+    Route::get('/create', 'create')->name('create');
+    Route::post('/store', 'store')->name('store');
+    Route::get('/edit/{id}', 'edit')->name('edit');
+    Route::post('/update/{id}', 'update')->name('update');
+    Route::get('/show/{id}', 'show')->name('show');
+    Route::get('/destroy/{id}', 'destroy')->name('destroy');
+});
+//..................... Admin Project Image Gallery Routes........................
+Route::prefix('/image')->name('image.')->controller(ImageController::class)->group(function () {
+    Route::get('/{pid}', 'index')->name('index');
+    Route::post('/store/{pid}', 'store')->name('store');
+    Route::get('/destroy/{pid}/{id}', 'destroy')->name('destroy');
+
+});
+});
+
