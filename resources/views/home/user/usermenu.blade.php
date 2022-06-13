515f9bd851b1c1810643de3d9688a6038ee2534b
Esraydin <aydinesra000@gmail.com>
2022-06-13 10:55:21 +0300

    Home User

--- resources/views/home/user/usermenu.blade.php
+++ resources/views/home/user/usermenu.blade.php
@@ -3,6 +3,6 @@
     <li><a href="/myprojects">My Evaluations</a></li>
     <li><a href="/myevaluations">My Projects</a></li>
     <li><a href="/myreviews">My Reviews</a></li>
-    <li><a href="/mymessages">My Messages</a></li>
+    <li><a href="/mymessages">My Messages.</a></li>
 
 </ul>
